501071838709a725c3535272216b9fea0ec05708
Evgeniia Semushina <semajenya96@gmail.com>
2023-03-14 22:03:17 +0200

    added counter in ingredient container

--- src/components/burger-ingredient/index.jsx
+++ src/components/burger-ingredient/index.jsx
@@ -1,11 +1,13 @@
 import s from './style.module.css';
 import sel from 'classnames';
-import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import { Counter, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 
 
-export const BurgerIngredient = ({ ...data }) => {
+export const BurgerIngredient = ({ ...data}) => {
     return (
+        
         <div className={sel(s.ingredient__conteiner, 'mb-10')}>
+            <Counter count={1} size="default" extraClass={sel(s.counter, "m-1")} />
             <img src={data.image} alt={data.name} className={s.image} />
             <p className={sel(s.price, 'text text_type_digits-default', 'mt-1', 'mb-1')}>{data.price} <CurrencyIcon type="primary" /></p>
             <h3 className={sel(s.title, 'text text_type_main-default')}>{data.name}</h3>
