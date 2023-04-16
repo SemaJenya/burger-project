affe71c2e7ea5ab6fbe3f4417a2f84d9db661379
Evgeniia Semushina <semajenya96@gmail.com>
2023-04-16 18:48:27 +0300

    ingredients-id deleted dublication elements

--- src/pages/ingredients-id/ingredients-id.jsx
+++ src/pages/ingredients-id/ingredients-id.jsx
@@ -3,12 +3,14 @@ import sel from 'classnames';
 import s from './style.module.css';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { IngredientDetails } from '../../components/ingredient-details';
 
 
 
 export const IngredientsID = () => {
     const params = useParams();
     const ingredientID = params.id;
+    
 
     const ingredients = useSelector(state => state.ingredientsStore.data);
     const currentIngredient = ingredients?.find(item => `${item._id }`=== ingredientID) || {}
@@ -17,27 +19,7 @@ export const IngredientsID = () => {
         <div className={s.page}>
             <div className={s.container}>
                 <h2 className={sel(s.title, 'text text_type_main-large')}>Детали ингредиента</h2>
-                <img src={currentIngredient?.image_large}/>
-                <p className={sel(s.subtitle, 'text text_type_main-medium mb-8')}>{currentIngredient?.name}</p>
-
-                <ul className={s.calories}>
-                    <li className={s.nutrients}>
-                        <p className={sel( s.nutrients__type ,'text text_type_main-default text_color_inactive')}>Калории,ккал</p>
-                        <p className={sel(s.nutrients__quantity, 'text text_type_digits-default text_color_inactive')}>{currentIngredient?.calories}</p>
-                    </li>
-                    <li className={s.nutrients}>
-                        <p className={sel( s.nutrients__type ,'text text_type_main-default text_color_inactive')}>Белки, г</p>
-                        <p className={sel(s.nutrients__quantity, 'text text_type_digits-default text_color_inactive')}>{currentIngredient?.proteins}</p>
-                    </li>
-                    <li className={s.nutrients}>
-                        <p className={sel( s.nutrients__type ,'text text_type_main-default text_color_inactive')}>Жиры, г</p>
-                        <p className={sel(s.nutrients__quantity, 'text text_type_digits-default text_color_inactive')}>{currentIngredient?.fat}</p>
-                    </li>
-                    <li className={s.nutrients}>
-                        <p className={sel( s.nutrients__type ,'text text_type_main-default text_color_inactive')}>Углеводы, г</p>
-                        <p className={sel(s.nutrients__quantity, 'text text_type_digits-default text_color_inactive')}>{currentIngredient?.carbohydrates}</p>
-                    </li>
-                </ul>    
+                <IngredientDetails />      
             </div>        
        </div>
     )
