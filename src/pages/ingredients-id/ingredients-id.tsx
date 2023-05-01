d16c03c0ea05e0a3ec1ed28748179ec4691a2660
Evgeniia Semushina <semajenya96@gmail.com>
2023-05-02 01:30:45 +0300

    typed ingredientsID

--- src/pages/ingredients-id/ingredients-id.tsx
+++ src/pages/ingredients-id/ingredients-id.tsx
@@ -1,20 +1,12 @@
 import { useEffect, useRef, useState } from 'react';
 import sel from 'classnames';
 import s from './style.module.css';
-import { useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 import { IngredientDetails } from '../../components/ingredient-details';
 
 
 
 export const IngredientsID = () => {
-    const params = useParams();
-    const ingredientID = params.id;
-    
 
-    const ingredients = useSelector(state => state.ingredientsStore.data);
-    const currentIngredient = ingredients?.find(item => `${item._id }`=== ingredientID) || {}
-   
     return (
         <div className={s.page}>
             <div className={s.container}>
