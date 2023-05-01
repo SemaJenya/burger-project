c116064a798c9bd29befdfd935cd23d5b2dcce22
Evgeniia Semushina <semajenya96@gmail.com>
2023-05-02 01:05:44 +0300

    typed modalOverlay

--- src/components/modal-overlay/index.tsx
+++ src/components/modal-overlay/index.tsx
@@ -1,9 +1,11 @@
 import PropTypes from 'prop-types';
 import s from './style.module.css';
 
+type TModalOverlay = {
+    onClick: () => void;
+} 
 
-
-export const ModalOverlay = ({onClick}) => {
+export const ModalOverlay: React.FC<TModalOverlay> = ({onClick}) => {
     return (
         <div className={s.overlay} onClick={onClick}>
 
