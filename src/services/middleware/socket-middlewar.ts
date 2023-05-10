d91ee89a7b2ad4b8f87924c21eb3c136cf8f779e
Evgeniia Semushina <semajenya96@gmail.com>
2023-05-11 00:41:55 +0300

    typed middleware

--- src/services/middleware/socket-middlewar.ts
+++ src/services/middleware/socket-middlewar.ts
@@ -1,15 +1,20 @@
 
+type TAction = {
+    payload: string
+}
+
+
 // ф-я созания мидлвара
-export const socketMiddleware = (wsActions) => {   //урл будем передавать в экшене для того, чтобы переиспользовать мидлвар
-  return store => {   //сам мидлвар в качестве параметра принимает стор и имеет к нему доступ
-    let socket = null;
+export const socketMiddleware = (wsActions: any) => {   //урл будем передавать в экшене для того, чтобы переиспользовать мидлвар
+  return (store: any) => {   //сам мидлвар в качестве параметра принимает стор и имеет к нему доступ
+    let socket: any = null;
     let wsUrl = '';
     let reconnectTimer = 0;
     let isConnected = false;
     let countConnecting = 0;
     const MAX_RECONNECTING = 5;
 
-    return next => action => {    //это экшен
+    return (next: any) => (action: TAction)=> {    //это экшен
       const { dispatch } = store;  //в сторе есть методы диспатч и гет стейт и мы можем их оттуда доставать
       const { wsConnect, wsDisconnect, wsConnecting, wsOpen, wsClose, wsError, wsMessage, wsSend } = wsActions;
 
@@ -22,21 +27,21 @@ export const socketMiddleware = (wsActions) => {   //урл будем пере�
       }
 
       if (socket) {
-        socket.onopen = event => {
+        socket.onopen = (event: any): void => {
           dispatch(wsOpen());
         };
 
-        socket.onerror = event => {
+        socket.onerror = (event: any) => {
           dispatch(wsError(event.code?.toString()));
         };
 
-        socket.onmessage = event => {
+        socket.onmessage = (event: any) => {
           const { data } = event;
           const parsedData = JSON.parse(data);
           dispatch(wsMessage(parsedData));
         };
         
-        socket.onclose = event => {
+        socket.onclose = (event: any) => {
           console.log('Im is on close');
           if (event.code !== 1000) {
             console.log('error close not 1000');
@@ -55,11 +60,11 @@ export const socketMiddleware = (wsActions) => {   //урл будем пере�
 
         };
 
-        if (wsSend.match(action)) {
-          // const message = '343434343';
-          console.log(action.payload);
-          // socket.send(JSON.stringify(message));
-        }
+        // if (wsSend.match(action)) {
+        //   // const message = '343434343';
+        //   console.log(action.payload);
+        //   // socket.send(JSON.stringify(message));
+        // }
       }
       
       
