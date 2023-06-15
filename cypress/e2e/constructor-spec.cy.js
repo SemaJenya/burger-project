81707c21152b19a78cc4c58da8c13ad24ee808ac
Evgeniia Semushina <semajenya96@gmail.com>
2023-06-15 17:47:08 +0300

    refactoring cypress tests

--- cypress/e2e/constructor-spec.cy.js
+++ cypress/e2e/constructor-spec.cy.js
@@ -4,6 +4,13 @@ const dataUser = {
   password: 'hardcarry'
 }
 
+const constructor = '[class^=style_constructor__inside__]';
+const bunElement = 'Флюоресцентная булка R2-D3';
+const sauceElement = 'Соус Spicy-X';
+const counter = '[class^=counter__num]';
+const modal = 'div [class^=style_modal__]';
+
+
 describe('constructor test', function () {
 
   beforeEach(() => {
@@ -19,20 +26,20 @@ describe('constructor test', function () {
   it('should open main page', () => {
     cy.contains('Соберите бургер');
     cy.contains('Выберете булку');
-    cy.contains('Соус Spicy-X');
-    cy.get('[class^=style_constructor__inside__]').not('[class^=style_inside__item__]');
+    cy.contains(sauceElement);
+    cy.get(constructor).not('[class^=style_inside__item__]');
     cy.get('[class^=style_ingredient__conteiner__').should('have.not', 'div [class^=counter]');
   })
 
 
   it('should open and close modal window', () => {
-    cy.contains('Соус Spicy-X').click();
+    cy.contains(sauceElement).click();
     cy.contains('Детали ингредиента');
-    cy.get('div [class^=style_modal__]').contains('Соус Spicy-X');
+    cy.get(modal).contains(sauceElement);
     cy.location().should((loc) => {
       expect(loc.href).to.include('ingredients/643d69a5c3f7b9001cfa0942')
     })
-    cy.get('[class=style_close__Krfgk]').click();
+    cy.get(modal).find('button').click();
     cy.url().should('eq', 'http://localhost:3000/');
   })
 
@@ -40,37 +47,37 @@ describe('constructor test', function () {
 
 
   it('should handle drag and drop ingredient', () => {
-    cy.contains('Соус Spicy-X').trigger('dragstart');
-    cy.get('[class^=style_constructor__inside__]').trigger('drop');
-    cy.get('[class^=style_constructor__inside__]').contains('Соус Spicy-X');
+    cy.contains(sauceElement).trigger('dragstart');
+    cy.get(constructor).trigger('drop');
+    cy.get(constructor).contains(sauceElement);
     cy.get('div [class^=style_inside__item__]').should('have.length', 1);
-    cy.contains('Соус Spicy-X').find('[class^=counter__num]').contains('1');
+    cy.contains(sauceElement).find(counter).contains('1');
 
 
     cy.contains('Соус с шипами Антарианского плоскоходца').trigger('dragstart');
-    cy.get('[class^=style_constructor__inside__]').trigger('drop');
+    cy.get(constructor).trigger('drop');
     cy.get('div [class^=style_inside__item__]').should('have.length', 2)
 
-    cy.contains('Соус Spicy-X').trigger('dragstart');
-    cy.get('[class^=style_constructor__inside__]').trigger('drop');
-    cy.contains('Соус Spicy-X').find('[class^=counter__num]').contains('2');
+    cy.contains(sauceElement).trigger('dragstart');
+    cy.get(constructor).trigger('drop');
+    cy.contains(sauceElement).find(counter).contains('2');
   })
 
   it('should handle drag and drop bun', () => {
     cy.get('[class^=constructor-element__text]').contains('Выберете булку');
-    cy.contains('Флюоресцентная булка R2-D3').trigger('dragstart');
-    cy.get('[class^=style_constructor__inside__]').trigger('drop');
-    cy.get('[class^=constructor-element__text]').contains('Флюоресцентная булка R2-D3');
+    cy.contains(bunElement).trigger('dragstart');
+    cy.get(constructor).trigger('drop');
+    cy.get('[class^=constructor-element__text]').contains(bunElement);
 
-    cy.contains('Флюоресцентная булка R2-D3').find('[class^=counter__num]').contains('2');
+    cy.contains(bunElement).find(counter).contains('2');
   })
 
   it('should open order modal window', () => {
-    cy.contains('Флюоресцентная булка R2-D3').trigger('dragstart');
-    cy.get('[class^=style_constructor__inside__]').trigger('drop');
+    cy.contains(bunElement).trigger('dragstart');
+    cy.get(constructor).trigger('drop');
 
-    cy.contains('Соус Spicy-X').trigger('dragstart');
-    cy.get('[class^=style_constructor__inside__]').trigger('drop');
+    cy.contains(sauceElement).trigger('dragstart');
+    cy.get(constructor).trigger('drop');
 
     cy.get('button').contains('Оформить заказ').click();
 
@@ -85,10 +92,10 @@ describe('constructor test', function () {
 
     cy.get('button').contains('Оформить заказ').click();
     cy.wait(15000);
-    cy.get('div [class^=style_modal__]').contains('идентификатор заказа');
+    cy.get(modal).contains('идентификатор заказа');
     cy.contains('Ваш заказ начали готовить');
 
-    cy.get('div [class^=style_modal__]').find('button').click();
+    cy.get(modal).find('button').click();
 
   })
 });
